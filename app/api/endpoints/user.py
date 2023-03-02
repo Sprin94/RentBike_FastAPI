bc3935f29340ec8bda5dea4305f689484228bd78
Andrei Rostovtsev <105871945+Sprin94@users.noreply.github.com>
2023-03-02 12:03:27 +0800

    add users/me endpoint

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,13 +1,21 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.api.utils.security import get_current_active_user
 from app.db.schemas.user import UserCreate
 from app.db.crud.user import UserCrud
-
+from app.db.schemas.user import UserBaseInDB
+from app.db.models.user import User
 
 router = APIRouter()
 
 
-@router.post('')
+@router.post('', response_model=UserBaseInDB)
 async def user_create(user: UserCreate,
                       crud: UserCrud = Depends()):
     result = await crud.create_user(user)
     return result
+
+
+@router.get('/me', response_model=UserBaseInDB)
+async def get_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
